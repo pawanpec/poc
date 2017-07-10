7f8d581156b4ee8ca58985914e626fe3d4339568
Pawan Goyal <you@example.com>
2017-07-10 14:35:53 +0530

    sudo code to update  actual data

--- src/main/java/ReadExcelWithFormula.java
+++ src/main/java/ReadExcelWithFormula.java
@@ -106,7 +106,6 @@ public class ReadExcelWithFormula {
 					}
 				}
 	        }
-	        System.out.println(inputData);
 	        HSSFFormulaEvaluator.evaluateAllFormulaCells(workbook);
 	        
 	        inp.close();
